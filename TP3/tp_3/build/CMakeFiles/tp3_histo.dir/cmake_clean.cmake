b51ccba4dc7d581ffcf32e4bbb79274b5301ab3d
Youssef  NIDABRAHIM <youssef.nidabrahim@etu.uca.fr>
2018-10-22 15:21:05 +0200

    TP3

---
+++ TP3/tp_3/build/CMakeFiles/tp3_histo.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "tp3_histo.pdb"
+  "tp3_histo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tp3_histo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ TP3/tp_3/build/CMakeFiles/tp3_test.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "tp3_test.pdb"
+  "tp3_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/tp3_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
